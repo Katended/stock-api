e15eace41a3b7b7e6eb52ebfb311e9ebc1b40ea4
David Katende <katendedavidjoga@gmail.com>
2023-07-07 08:56:47 +0300

    Fix linters error

--- src/test/Details.test.js
+++ src/test/Details.test.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import store from '../redux/store';
+import Store from '../redux/Store';
 import Rates from '../components/Rates';
 
 describe('Rates component', () => {
   it('renders Rates correctly', () => {
-    const tree = render(<Provider store={store}><Router><Rates /></Router></Provider>);
+    const tree = render(<Provider store={Store}><Router><Rates /></Router></Provider>);
     expect(tree).toMatchSnapshot();
   });
 });
